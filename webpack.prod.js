1dd0e3d2fda4ebbfc9a221e0452b503a62ada163
SparkElf <374656045@qq.com>
2021-12-24 23:56:26 +0800

    master

--- webpack.prod.js
+++
@@ -1,6 +0,0 @@
-import { merge } from "webpack-merge";
-import { common } from "./webpack.common.js";
-import { CleanPlugin } from "webpack";
-export default merge(common, {
-  plugins: [new CleanPlugin()],
-});
